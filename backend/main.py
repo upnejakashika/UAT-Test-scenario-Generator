6ef6b569872f2be131a99b1336900658ae1341f5
upnejakashika <upnejakashika@gmail.com>
2026-04-25 16:12:41 +0530

    Added main backend file

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,20 @@
+from fastapi import FastAPI, UploadFile, File
+import shutil, os
 
+app = FastAPI()
+
+UPLOAD_DIR = "data/uploads/"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+@app.get("/")
+def home():
+    return {"message": "AI Test Agent Running"}
+
+@app.post("/upload-srs/")
+async def upload_srs(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {"message": "File uploaded successfully"}
