0358c49e7c4b8b9b2fc85b322ec33947678ecc3b
upnejakashika <upnejakashika@gmail.com>
2026-04-26 17:51:11 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,34 @@
+
+Kashika Upneja <upnejakashika@gmail.com>
+5:24 PM (23 minutes ago)
+to me
+
 from fastapi import FastAPI, UploadFile, File
-import shutil, os
+from backend.parser import extract_text_from_pdf
+from backend.llm import generate_test_scenarios
+import shutil
+import os
 
 app = FastAPI()
 
-UPLOAD_DIR = "data/uploads/"
-os.makedirs(UPLOAD_DIR, exist_ok=True)
-
-@app.get("/")
-def home():
-    return {"message": "AI Test Agent Running"}
+UPLOAD_FOLDER = "backend/data/uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.post("/upload-srs/")
 async def upload_srs(file: UploadFile = File(...)):
-    file_path = os.path.join(UPLOAD_DIR, file.filename)
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
 
+    # Save file
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    return {"message": "File uploaded successfully"}
+    # Extract text
+    extracted_text = extract_text_from_pdf(file_path)
+
+    # Generate scenarios
+    scenarios = generate_test_scenarios(extracted_text)
+
+    return {
+        "message": "File processed successfully",
+        "scenarios": scenarios
+    }
